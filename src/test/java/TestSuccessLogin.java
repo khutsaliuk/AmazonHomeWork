b5663efc5117081bbc012748a2dbf76bae142ce3
Konstantyn Hutsaliuk <paskasomka@gmail.com>
2023-12-28 21:26:00 +0200

    first commit

--- src/test/java/TestSuccessLogin.java
+++ src/test/java/TestSuccessLogin.java
@@ -7,9 +7,10 @@ import static com.codeborne.selenide.Selenide.open;
 public class TestSuccessLogin extends InitialSelenide {
     @Test
     public void testSuccessLogin(){
-        open("https://www.amazon.com/");
+        open("https://wtools.io/ru/check-my-user-agent");
 /*        before();
      new MainPage().clickOnSignIn().typeEmail("sadsa").clickOnContinueBtn().
              typePassword("123").clickSignInBtn();*/
     }
 }
+//42eb6abf0f1e4daaa0ab0a7dc6ff407f
